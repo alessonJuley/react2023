c6081a28b37bef74b85c10f29be68c465eecf704
alessonJuley <alessonabao@gmail.com>
2023-03-01 23:19:44 +1300

    lifting state up

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 
 function PostList() {
 
+          const [enteredbody, setEnteredBody] = useState("");
+          const [enteredAuthor, serEnteredAuthor] = useState("");
+
+          function bodyChangeHandler(event){
+                    setEnteredBody(event.target.value);
+          }
+          function authorChangeHandler(event){
+                    serEnteredAuthor(event.target.value);
+          }
           return (
           <>
-                    <NewPost />
+                    <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
                               <ul className={classes.posts}>
                                         <Post author="Phoenix" body="'Almost Christmas' means it wasn't Christmas!"/>
-                                        <Post author="Grant" body="Give it up for day 15!"/>
+                                        <Post author={enteredAuthor} body={enteredbody}/>
                               </ul>
           </>);
 }
