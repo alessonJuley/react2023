34ff07847a270915b3178f09d19de4930ce464a3
alessonJuley <alessonabao@gmail.com>
2023-03-01 23:50:14 +1300

    special 'children' prop

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
+import Modal from "./Modal";
 
 function PostList() {
 
@@ -16,11 +17,16 @@ function PostList() {
           }
           return (
           <>
-                    <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
-                              <ul className={classes.posts}>
-                                        <Post author="Phoenix" body="'Almost Christmas' means it wasn't Christmas!"/>
-                                        <Post author={enteredAuthor} body={enteredbody}/>
-                              </ul>
+                    <Modal>
+                              <NewPost 
+                                        onBodyChange={bodyChangeHandler} 
+                                        onAuthorChange={authorChangeHandler}
+                              />
+                    </Modal>
+                    <ul className={classes.posts}>
+                              <Post author="Phoenix" body="'Almost Christmas' means it wasn't Christmas!"/>
+                              <Post author={enteredAuthor} body={enteredbody}/>
+                    </ul>
           </>);
 }
 
