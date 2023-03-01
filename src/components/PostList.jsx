d08f6a49e6688164f41c29fdcf1f6dd2d975b2c0
alessonJuley <alessonabao@gmail.com>
2023-03-01 22:55:03 +1300

    added state feature

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,17 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 
 function PostList() {
 
-          return (<ul className={classes.posts}>
-                    <Post author="Phoenix" body="'Almost Christmas' means it wasn't Christmas!"/>
-                    <Post author="Grant" body="Give it up for day 15!"/>
-          </ul>);
+          return (
+          <>
+                    <NewPost />
+                              <ul className={classes.posts}>
+                                        <Post author="Phoenix" body="'Almost Christmas' means it wasn't Christmas!"/>
+                                        <Post author="Grant" body="Give it up for day 15!"/>
+                              </ul>
+          </>);
 }
 
 export default PostList;
