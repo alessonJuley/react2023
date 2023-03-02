6b1a8c2840c830d651e50fb675e0dc89a335eb1a
alessonJuley <alessonabao@gmail.com>
2023-03-02 20:58:25 +1300

    added useEffect()

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
@@ -8,7 +8,24 @@ function PostList({isPosting, onStopPosting}) {
 
           const [posts, setPosts] = useState([]);
 
+          useEffect(() => {
+                    async function fetchPosts(){
+                              const response = await fetch("http://localhost:8080/posts");
+                              const resData = await response.json();
+                              setPosts(resData.posts);
+                    }
+
+                    fetchPosts();
+          }, []);
+
           function addPostHandler(postData){
+                    fetch("http://localhost:8080/posts", {
+                              method: "POST",
+                              body: JSON.stringify(postData),
+                              headers: {
+                                        "Content-Type": "application/json"
+                              }
+                    });
                     setPosts((existingPosts) => [postData, ...existingPosts]);
           }
 
