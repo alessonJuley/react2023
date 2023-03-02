98784db69f45e35446dc36317e0a74f11928d5e3
alessonJuley <alessonabao@gmail.com>
2023-03-02 19:22:40 +1300

    handling form submissions

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
@@ -6,23 +5,11 @@ import Modal from "./Modal";
 
 function PostList({isPosting, onStopPosting}) {
 
-          const [enteredbody, setEnteredBody] = useState("");
-          const [enteredAuthor, setEnteredAuthor] = useState("");
-
-          function bodyChangeHandler(event){
-                    setEnteredBody(event.target.value);
-          }
-          function authorChangeHandler(event){
-                    setEnteredAuthor(event.target.value);
-          }
-
           let modalContent;
           
           if(isPosting){
                     modalContent = <Modal onClose={onStopPosting}>
                     <NewPost 
-                              onBodyChange={bodyChangeHandler} 
-                              onAuthorChange={authorChangeHandler}
                               onCancel={onStopPosting}
                     />
                     </Modal>
@@ -33,7 +20,6 @@ function PostList({isPosting, onStopPosting}) {
                     {modalContent}
                     <ul className={classes.posts}>
                               <Post author="Phoenix" body="'Almost Christmas' means it wasn't Christmas!"/>
-                              <Post author={enteredAuthor} body={enteredbody}/>
                     </ul>
           </>);
 }
