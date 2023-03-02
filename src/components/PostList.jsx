efbcaea7fa49db8f229720cb37f6053a284ffb66
alessonJuley <alessonabao@gmail.com>
2023-03-02 21:15:36 +1300

    handle loading state

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -6,13 +6,17 @@ import Modal from "./Modal";
 
 function PostList({isPosting, onStopPosting}) {
 
-          const [posts, setPosts] = useState([]);
+          const [posts, setPosts] = useState(false);
+
+          const [isFetching, setIsFetching] = useState(false);
 
           useEffect(() => {
                     async function fetchPosts(){
+                              setIsFetching(true);
                               const response = await fetch("http://localhost:8080/posts");
                               const resData = await response.json();
                               setPosts(resData.posts);
+                              setIsFetching(false);
                     }
 
                     fetchPosts();
@@ -43,16 +47,21 @@ function PostList({isPosting, onStopPosting}) {
           return (
           <>
                     {modalContent}
-                    {posts.length > 0 && (
+                    {!isFetching && posts.length > 0 && (
                     <ul className={classes.posts}>
                               {posts.map((post) => <Post key={post.body} author={post.author} body={post.body}/>)}
                     </ul>)
                     }
-                    {posts.length === 0 && 
+                    {!isFetching && posts.length === 0 && 
                     <div className={classes.noPost}>
                               <h2>There are no post yet.</h2>
                               <p>Start adding some!</p>
                     </div>}
+                    {isFetching && <div className={classes.loading}>
+                              <h2>Loading posts...</h2>
+                    </div>
+
+                    }
           </>);
 }
 
