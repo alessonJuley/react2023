9570dc38febf942098b2e9973763cd374038da0d
alessonJuley <alessonabao@gmail.com>
2023-03-05 00:40:23 +1300

    added more info comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,5 @@
 import classes from "./Post.module.css";
-
+// this part is about the actual post containing the author and text
 function Post(props) {
 
           return (<li className={classes.post}>
