2d2f985ebdaa997db9e859bfa938cd63e284736b
alessonJuley <alessonabao@gmail.com>
2023-03-06 17:10:24 +1300

    added comment

--- src/routes/RootLayout.jsx
+++ src/routes/RootLayout.jsx
@@ -1,6 +1,6 @@
 import MainHeader from "../components/MainHeader";
 import { Outlet } from "react-router-dom";
-
+// root layout is the home route in this poject
 function RootLayout(){
           return <>
                     <MainHeader />
